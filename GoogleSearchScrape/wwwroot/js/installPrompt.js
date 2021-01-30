ca60452e90bfa3bde89960287f5c55fa4b78808d
Jayarrowz <arrowzflame321@gmail.com>
2021-01-30 18:22:09 +0000

    Commit wwwroot

---
+++ GoogleSearchScrape/wwwroot/js/installPrompt.js
@@ -0,0 +1,7 @@
+﻿window.addEventListener('beforeinstallprompt', function (e) {
+    console.log("PWA Init Event");
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    window.PWADeferredPrompt = e;
+});
---
+++ GoogleSearchScrape/wwwroot/js/pwa.js
@@ -0,0 +1,27 @@
+﻿var layoutRef = null;
+const blazorInstallMethod = 'ShowInstallPrompt';
+
+export function setLayoutRef(ref) {
+    layoutRef = ref;
+}
+export function showAddToHomeScreen() {
+    if (layoutRef == null) {
+        setTimeout(showAddToHomeScreen, 1000);
+    } else {
+        layoutRef.invokeMethodAsync(blazorInstallMethod)
+            .then(function () { layoutRef = null; }, function (er) { setTimeout(showAddToHomeScreen, 1000); });
+    }
+}
+
+export function installPWA() {
+    var prompt = window.PWADeferredPrompt;
+    if (prompt) {
+        prompt.prompt();
+        prompt.userChoice
+            .then(function (choiceResult) {
+                window.PWADeferredPrompt = null;
+            });
+    }
+}
+
+showAddToHomeScreen();
