62d12d819b7c87ba14d80a749e4c0c36fd7d7319
Jayarrowz <arrowzflame321@gmail.com>
2021-01-31 13:55:42 +0000

    Update ScraperExtensions docs

--- GoogleSearchScrape.Scrapers/Extensions/ScraperExtensions.cs
+++ GoogleSearchScrape.Scrapers/Extensions/ScraperExtensions.cs
@@ -5,6 +5,11 @@ namespace GoogleSearchScrape.Scrapers.Extensions
 {
     public static class ScraperExtensions
     {
+        /// <summary>
+        /// Adds the <see cref="ScraperService"/> to <see cref="IServiceCollection"/>
+        /// </summary>
+        /// <param name="collection">The service collection.</param>
+        /// <returns>The <see cref="IServiceCollection"/> with the <see cref="ScraperService"/></returns>
         public static IServiceCollection AddScraperService(this IServiceCollection collection)
         {
             return collection.AddHostedService<ScraperService>();
